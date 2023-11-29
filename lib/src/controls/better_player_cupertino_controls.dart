12efe8cdcdd64cf01a4f586839431d97fc452d4c
Pierre Sabbagh <p.sabbagh@softimpact.net>
2023-11-29 10:14:14 +0200

    update cupertino controls

--- lib/src/controls/better_player_cupertino_controls.dart
+++ lib/src/controls/better_player_cupertino_controls.dart
@@ -748,7 +748,7 @@ class _BetterPlayerCupertinoControlsState extends BetterPlayerControlsState<Bett
                   horizontal: buttonPadding,
                 ),
                 decoration: BoxDecoration(color: backgroundColor),
-                child: child)));
+                child: controlsNotVisible ? SizedBox.shrink() : child)));
   }
 
   Widget _buildPipButton(
